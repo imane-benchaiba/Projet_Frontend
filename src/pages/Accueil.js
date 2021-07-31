35a09908397975f3189b8bf1f4717eac0f65ea7a
imane-benchaiba <imyben.bi@gmail.com>
2021-07-31 04:09:29 -0700

    MAJ Accueil

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Image1 from "../img/cheremamie.jpg";
-import Image2 from "../img/sharko.jpg";
-import Image3 from "../img/puzzle.jpg";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import CardBook from "../components/Book/CardBook";
 
 const Accueil = () => {
@@ -37,12 +34,13 @@ const Accueil = () => {
           className="search__input"
           type="search"
           id="search"
-          placeholder="Chercher un livre, un auteur..."
+          placeholder="Chercher un livre..."
           onChange={searchBook}
         ></input>{" "}
       </div>
 
-      {search === false && (
+      {
+      /*search === false && (
         <div className="third__container">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -72,7 +70,8 @@ const Accueil = () => {
             <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
           </svg>
         </div>
-      )}
+      )*/
+      }
       {search && (
         <div className="books-thread-main">
           <ul>
